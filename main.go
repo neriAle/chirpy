0c0f9afa48b51d977e2777c4c84f0c1e08dc2f5b
Alessandro Neri <a.neri@student.tudelft.nl>
2025-08-06 15:55:22 +0200

    👔 Added db loading through secret connection string

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import(
+	"github.com/neriAle/chirpy/internal/database"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
 func main() {
-	startServer()
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbQueries := database.New(db)
+	startServer(dbQueries)
 }
